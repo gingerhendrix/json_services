ee6c10a74f1717f842c4cab322ccad1f2c8b9b77
Gareth Andrew <gingerhendrix@hotmail.com>
2008-11-27 11:22:57 +0000

    Added cache expiry to github service

--- lib/json_services/github.rb
+++ lib/json_services/github.rb
@@ -1,14 +1,14 @@
 
 np_namespace "github" do |ns|
-  ns.route 'user_info', [:username], {:cache => false} do |username|
+  ns.route 'user_info', [:username], {:cache_expiry => 24.hours} do |username|
     GitHub::API.user(username)
   end
   
-  ns.route "commits", [:username, :repo] do |username, repo|
+  ns.route "commits", [:username, :repo], {:cache_expiry => 24.hours} do |username, repo|
     GitHub::API.commits(username, repo)
   end
   
-  ns.route "commit", [:username, :repo, :id] do |username, repo, id|
+  ns.route "commit", [:username, :repo, :id], {:cache_expiry => 24.hours} do |username, repo, id|
     GitHub::API.commit(username, repo, id)
   end
 end
